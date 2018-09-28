f019a353e8999874df1ad0a4452a88d526ff0a62
Qin <qinnan851@hotmail.com>
2018-09-28 23:22:04 +0800

    1

--- xiangrikui/csv.py
+++ xiangrikui/csv.py
@@ -2,8 +2,7 @@ import time
 import os
 
 result = []
-with open(r'D:\PycharmProjects\spider\xiangrikui\222 - 副本.txt',
-          mode='r', encoding='utf-8') as f:
+with open(r'222 - 副本.txt', mode='r', encoding='utf-8') as f:
     for line in f:
         result.append(line)
 x = 0
@@ -12,6 +11,24 @@ print(total)
 
 title = ''
 content = ''
-for x in range(0, total):
-    if result[x] == '----------\n':
-        print(result[x + 1])
+# for x in range(0, total):
+#     if result[x] != '----------\n':
+#         print(result[x])
+ss = ''
+with open(r'222 - 副本.txt', mode='r', encoding='utf-8') as f:
+    ss = f.read()
+result = ss.split('----------\n')
+# with open(r'finnal1.txt', mode='a', encoding='utf-8') as ff:
+#     for a in result:
+#         ccc = a.replace('\n', '')
+#         ff.write(ccc)
+#         ff.write('\n')
+#         print(a)
+with open(r'finnal1.txt', mode='r', encoding='utf-8') as ff:
+    with open(r'finnal111.csv', mode='a', encoding='utf-8') as fff:
+        for a in ff.readlines():
+            x = a.replace(' ', ',', 1)
+            print(x)
+            fff.write(x)
+
+
--- xiangrikui/spider_everyone.py
+++ xiangrikui/spider_everyone.py
@@ -10,7 +10,7 @@ with open('spider_url.txt', mode='r', encoding='utf-8') as f:
 #url_list = ['http://www.xiangrikui.com/bxmingci/shehuibaoxian/20110112/85830.html']
 #url_list = ['http://www.xiangrikui.com/bxmingci/changyong/20110121/88097.htmll']
 with open('xiangrikui_knowlege.txt', mode='r', encoding='utf-8') as f:
-    ff = open('222.txt', mode='a', encoding='utf-8')
+    ff = open('333.txt', mode='a', encoding='utf-8')
     for link in url_list[0:]:
         link = link.strip()
         print(link)
@@ -25,19 +25,19 @@ with open('xiangrikui_knowlege.txt', mode='r', encoding='utf-8') as f:
         title = ''
         for x in range(0, aaa.__len__()):
             if aaa[x].attrs['class'][0] == 'ri_title_big':
-                sp = '----------\n'
-                print(sp)
-                ff.write(sp)
+                #sp = '----------\n'
+                #print(sp)
+                #ff.write(sp)
                 title = aaa[x].text
                 if aaa[x].text == '':
                     print(rt[0].get_text())
                     title = rt[0].get_text()
                 print(title)
-                ff.write(title + '\n')
+                ff.write('|' + title + '\n')
                 continue
             if aaa[x].attrs['class'][0] == 'ri_fill':
                 content = aaa[x].text
-                ff.write(content + '\n')
+                ff.write('^' + content + '\n')
                 print(content)
 ff.close()
 f.close()
